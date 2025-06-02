538d39ce50b5c0e86a77c2df42311ad7da62ff46
JohndoeEXE <102329284+JohndoeEXE@users.noreply.github.com>
2025-06-02 11:53:57 -0700

    Create proxies.js

---
+++ proxies.js
@@ -0,0 +1,10 @@
+// proxies.js - Proxy configuration file
+// Format: "host:port:username:password"
+// Add your proxies to this array
+
+module.exports = [
+    "pr.oxylabs.io:7777:customer-slit1_Pklwu-cc-US-sessid-0416505364-sesstime-10:jmMw39knaMNXmCH_",
+    // Add more proxies here as needed
+    // "proxy2.example.com:8080:username2:password2",
+    // "proxy3.example.com:3128:username3:password3",
+];
